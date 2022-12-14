4e43ca005753477378dcd7a3677b3fdf6f50916f
kaajc <jgress1802@gmail.com>
2022-12-14 15:30:25 -0600

    Started about section

--- navBar.js
+++ navBar.js
@@ -1,3 +1,30 @@
-/*
-    This is a JavaScript file that produces the different styles and animations on the navigation bar
-*/
+/* This is a JavaScript file that produces the different styles and animations on the navigation bar */
+
+
+function home(){
+
+}
+
+function aboutMe(){
+
+}
+
+function projects(){
+
+}
+
+function contact(){
+
+}
+
+function github(){
+
+}
+
+function main(){
+
+}
+
+function linkedin(){
+
+}
