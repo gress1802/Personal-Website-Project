79a4bd4044f000a764f38449b516fd3fd40dcf45
kaajc <jgress1802@gmail.com>
2022-12-14 20:19:29 -0600

    digging into the about me section

--- navBar.js
+++ navBar.js
@@ -1,12 +1,21 @@
 /* This is a JavaScript file that produces the different styles and animations on the navigation bar */
 
+window.onscroll = function(){scroll()};
+function scroll(){
+    if(document.body.scrollTop>20 || document.documentElement.scrollTop > 20){ 
+        document.getElementById("navigation").style.top = "0";
+
+    }else{
+        document.getElementById("navigation").style.top = "-70px";
+    }
+}
 
 function home(){
-
+    document.getElementById("startPage").scrollIntoView(true);
 }
 
 function aboutMe(){
-
+    document.getElementById("aboutMe").scrollIntoView(true);
 }
 
 function projects(){
@@ -21,7 +30,7 @@ function github(){
 
 }
 
-function main(){
+function mail(){
 
 }
 
