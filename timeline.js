e6e0408f627b8cf8a455ad03b33784d77925ae29
kaajc <jgress1802@gmail.com>
2022-12-15 20:11:10 -0600

    Just need to add content to about me now!

---
+++ timeline.js
@@ -0,0 +1,38 @@
+//This is the code for the timeline icons
+var overlay = document.getElementById("overlay");
+
+function school(){
+    document.getElementById("firstModal").classList.add("active");
+    overlay.classList.add("active");
+}
+
+function motionVolleyball(){
+    document.getElementById("secondModal").classList.add("active");
+    overlay.classList.add("active");
+}
+
+function frontDesk(){
+    document.getElementById("thirdModal").classList.add("active");
+    overlay.classList.add("active");
+}
+
+function matteos(){
+    document.getElementById("thirdModal").classList.add("active");
+    overlay.classList.add("active");
+}
+
+function goodtimes(){
+    document.getElementById("thirdModal").classList.add("active");
+    overlay.classList.add("active");
+}
+
+function johnsonControls(){
+    document.getElementById("thirdModal").classList.add("active");
+    overlay.classList.add("active");
+}
+
+function exit(modal){ /* This is for exiting the modal */
+    var mod = document.getElementById(modal);
+    mod.classList.remove("active");
+    overlay.classList.remove("active");
+}   
